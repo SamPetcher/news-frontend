e1bdb75c616930b16697014570b5f2f5cd53d6c5
sampetcher <sampetcher@gmail.com>
2023-02-12 14:35:44 +0000

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from './Nav'
+import Articles from "./Articles"
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Articles />
+    </div>
+  )
+}
+
+export default App
