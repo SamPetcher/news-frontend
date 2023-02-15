e9a6e9ebd01f2098d7ad0b76f221179d7a1ec4ba
sam <sam@pop-os.localdomain>
2023-02-15 16:25:24 +0000

    Add list of articles view for site

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,22 +1,35 @@
 import Loader from "./Loader"
 import React, { useState } from 'react';
 import { useGetPostsQuery } from '../features/apiSlice'
+
 function Articles() {
-  // const [isLoading, setLoading] = useState(true);
   const {
-    data: posts,
+    data,
     isLoading,
     isSuccess,
     isError,
     error
-  } = useGetPostsQuery()
-  console.log(posts)
+  } = useGetPostsQuery();
+
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  let content
+  if (isLoading) content = <Loader />
+  else if (isError) content = error
+  else if (isSuccess) content = data.articles.map(post => (
+    <div key={post.id}>
+      <h1 className="font-sans text-lg  font-semibold">{post.title}</h1>
+      <a className="font-mono text-slate-500">{post.author}</a>
+      <a className="font-mono text-slate-500">{
+        new Date(post.created_at).toString().slice(0, 24)
+      }</a>
+    </div>
+  ))
+
   return (
-    (isLoading ?
-      <Loader />
-      :
-      <div>Loaded</div>)
-  )
+    <div>
+      {content}
+    </div>
+  );
 }
 
-export default Articles
+export default Articles;
