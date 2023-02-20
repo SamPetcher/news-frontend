cec361b637a2aec2e168289b7b6ed272b72bac47
Samuel Petcher <Sampetcher@gmail.com>
2023-02-20 16:17:51 +0000

    Refactor into ChakraUI

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import { Link as RouteLink } from "react-router-dom";
+import { Box, Flex } from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
+
 export default function Nav() {
   return (
-    <nav className="fixed top-0 w-full z-10 bg-slate-200  border-b border-zinc-500 ">
-
-      <div className="mx-auto container flex items-center justify-between px-4 py-3">
-        <a>Link</a>
-        <a>Link</a>
-        <a>Link</a>
-        <a>Link</a>
-      </div>
-    </nav>
-  )
+    <Flex
+      // marginX={'20%'}
+      width={"90%"}
+      padding={2}
+      marginBottom={10}
+      paddingBottom={5}
+      paddingTop={5}
+      bgGradient="linear(to-b, blue.200, gray.600)"
+      boxShadow={"2xl"}
+      borderTopRadius={0}
+      borderBottomRadius={"0.5em"}
+      justify={"space-around"}
+      color={"gray.100"}
+    >
+      <Link as={RouteLink} to={"/"}>
+        Climbing
+      </Link>
+      <Link as={RouteLink} to={"/"}>
+        Cooking
+      </Link>
+      <Link as={RouteLink} to={"/"}>
+        Fun
+      </Link>
+      <Link as={RouteLink} to={"/"}>
+        Coding
+      </Link>
+    </Flex>
+  );
 }
-
