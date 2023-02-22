327eb3454fb2d733b80152412588471c30ed55e1
Samuel Petcher <Sampetcher@gmail.com>
2023-02-22 18:39:11 +0000

    add optional filtering component to article list

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -7,19 +7,28 @@ import {
   Text,
 } from "@chakra-ui/react";
 import React from "react";
-import { Link as RouteLink } from "react-router-dom";
+import { Link as RouteLink, useParams } from "react-router-dom";
 import { useGetPostsQuery } from "../features/apiSlice";
 import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 
 function Articles() {
+  const params = useParams();
   const { data, isLoading, isSuccess, isError, error } = useGetPostsQuery();
 
   let content;
   if (isLoading) content = <Spinner />;
   else if (isError) content = error;
-  else if (isSuccess)
-    content = data.articles.map((post) => (
+  else if (isSuccess) {
+    let filteredTopics;
+    if (!params.topic) filteredTopics = data.articles;
+    else {
+      filteredTopics = [...data.articles].filter((article) => {
+        return article.topic === params.topic;
+      });
+    }
+
+    content = filteredTopics.map((post) => (
       <Card key={uuidv4()} marginBottom={1} padding={0} width={"90%"}>
         <CardHeader margin={0} padding={1}>
           <Link to={`article/${post.article_id}`} as={RouteLink} size={"md"}>
@@ -42,7 +51,12 @@ function Articles() {
         </CardBody>
       </Card>
     ));
-
+  }
+  /* const topicFilter = data.articles.filter(
+    (article) => article.topic === params
+  ); */
+  if (isSuccess) {
+  }
   return <>{content}</>;
 }
 
